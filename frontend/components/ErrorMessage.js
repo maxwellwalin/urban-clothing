cd7ccac7f9bc08d7a423c33647dc241e848ef35f
maxwell walin <maxwellwalin@gmail.com>
2021-12-28 21:21:56 -0800

    created custom useForm hook

--- frontend/components/ErrorMessage.js
+++ frontend/components/ErrorMessage.js
@@ -1,21 +1,6 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const ErrorStyles = styled.div`
-  padding: 2rem;
-  background: white;
-  margin: 2rem 0;
-  border: 1px solid rgba(0, 0, 0, 0.05);
-  border-left: 5px solid red;
-  p {
-    margin: 0;
-    font-weight: 100;
-  }
-  strong {
-    margin-right: 1rem;
-  }
-`;
-
 const DisplayError = ({ error }) => {
   if (!error || !error.message) return null;
   if (error.networkError && error.networkError.result && error.networkError.result.errors.length) {
@@ -45,5 +30,20 @@ DisplayError.defaultProps = {
 DisplayError.propTypes = {
   error: PropTypes.object,
 };
+
+const ErrorStyles = styled.div`
+  padding: 2rem;
+  background: white;
+  margin: 2rem 0;
+  border: 1px solid rgba(0, 0, 0, 0.05);
+  border-left: 5px solid red;
+  p {
+    margin: 0;
+    font-weight: 100;
+  }
+  strong {
+    margin-right: 1rem;
+  }
+`;
 
 export default DisplayError;

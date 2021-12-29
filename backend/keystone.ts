7d23c329449418af7ee3ce9e6d69139d6e02854d
maxwell walin <maxwellwalin@gmail.com>
2021-12-28 22:27:45 -0800

    added typescript types and schema to backend

---
+++ backend/keystone.ts
@@ -0,0 +1,63 @@
+import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-urban-clothing';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 30 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // add inital roles
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      async onConnect(keystone) {
+        if (process.argv.includes('--seed-data')) {
+          // await insertSeedData(keystone);
+        }
+      },
+    },
+    lists: createSchema({
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // graphql query
+      User: 'id name email',
+    }),
+  })
+);

ba91a450f428da3a2590cd5bd067cc550f54e475
maxwell walin <maxwellwalin@gmail.com>
2021-12-28 20:43:57 -0800

    updated _app with globalstyles, nprogress, and apolloprovider

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,26 +1,78 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
+
+import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
+  font-face {
+    font-family: 'radnika';
+    src: url('/static/radnikanext-medium-webfont.woff2')format('woff2');
+    font-weight: normal;
+    font-style: normal;
+    }
+
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
     box-sizing: border-box;
+    font-size: 62.5%;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--black);
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  button {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: '#ff0000',
   },
 }
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, apollo }) {
   return (
-    <>
+    <ApolloProvider client={apollo}>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </ApolloProvider>
   )
 }
+
+export default App;
